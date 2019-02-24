849982960592f098c509dad5adf16ae009aacdb9
Emmanuel <akpejionimi@yahoo.com>
2019-02-24 22:27:12 +0100

    modify css

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,40 @@
-const { src, dest, task, watch, parallel } = require('gulp');
-const terser = require('gulp-terser');
-const rename = require('gulp-rename');
-const concat = require('gulp-concat');
-const minifyCSS = require('gulp-csso');
-const browsersync = require ('browser-sync').create();
+// const { src, dest, task, watch, parallel } = require('gulp');
+// const terser = require('gulp-terser');
+// const rename = require('gulp-rename');
+// const concat = require('gulp-concat');
+// const minifyCSS = require('gulp-csso');
+// const browsersync = require ('browser-sync').create();
 
 
-function css() {
-    return src('css/*.css')
-        .pipe(minifyCSS())
-        .pipe(rename(function(path){
-           path.extname= '.min.css';
-        }))
-        .pipe(dest('build/css'));
-}
-function js() {
-  return src('js/*.js')
-    .pipe(concat('scripts.js'))
-    .pipe(dest('build/scripts'))
-    .pipe(rename('scripts.min.js'))
-    .pipe(terser())
-    .pipe(dest('build/scripts'));
-};
+// function css() {
+//     return src('css/*.css')
+//         .pipe(minifyCSS())
+//         .pipe(rename(function(path){
+//            path.extname= '.min.css';
+//         }))
+//         .pipe(dest('build/css'));
+// }
+// function js() {
+//   return src('js/*.js')
+//     .pipe(concat('scripts.js'))
+//     .pipe(dest('build/scripts'))
+//     .pipe(rename('scripts.min.js'))
+//     .pipe(terser())
+//     .pipe(dest('build/scripts'));
+// };
 
-function liveReload(){
-  browsersync.init({
-      server: {
-          baseDir:"./"
-      }
+// function liveReload(){
+//   browsersync.init({
+//       server: {
+//           baseDir:"./"
+//       }
 
-  });
-}
-function defaultTask(done){
-  browsersync.reload();
-  done();
-}
-watch('*',parallel(css, js, defaultTask));
+//   });
+// }
+// function defaultTask(done){
+//   browsersync.reload();
+//   done();
+// }
+// watch('*',parallel(css, js, defaultTask));
 
-exports.default = parallel(css,js,liveReload);
+// exports.default = parallel(css,js,liveReload);
